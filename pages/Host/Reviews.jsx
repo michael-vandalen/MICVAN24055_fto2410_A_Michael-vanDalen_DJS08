01159d8f404b7110e63c65b81d05ee8d05d74004
Michael van Dalen <michaelvdalen191@gmail.com>
2025-04-05 14:48:21 +0200

    Added most of the pages and routes

---
+++ pages/Host/Reviews.jsx
@@ -0,0 +1,3 @@
+export default function Reviews() {
+  return <h1>This is where the reviews show</h1>;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
-
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- *
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- *
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import Vans from "../pages/Vans/Vans";
+import VanDetail from "../pages/Vans/VanDetail";
+import Layout from "../components/Layout";
+import Income from "../pages/Host/Income";
+import Reviews from "../pages/Host/Reviews";
+import HostLayout from "../components/HostLayout";
+import Dashboard from "../pages/Host/Dashboard";
+import HostVans from "../pages/Host/HostVans";
+import HostVanDetail from "../pages/Host/HostVanDetail";
+import HostVanInfo from "../pages/Host/HostVanInfo";
+import HostVanPricing from "../pages/Host/HostVanPricing";
+import HostVanPhotos from "../pages/Host/HostVanPhotos";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
